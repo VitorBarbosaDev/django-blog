f6bfaf4452c19b86044994a5f97d92cc34b474a7
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-01-03 22:59:07 +0000

    add class based view

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s!j2jeyt$1cayd!+m1lvux-os%!&(r#-odfm6kajxy%rne--2='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 

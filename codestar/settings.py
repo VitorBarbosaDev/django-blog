3c82a0e4f90d2043376aced3cc2d634240211c10
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-01-04 13:30:37 +0000

    add templates

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,6 +33,9 @@ if os.path.isfile('env.py'):
     import env
 
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -61,7 +64,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
